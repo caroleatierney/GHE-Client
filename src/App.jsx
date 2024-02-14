b97dfdeef1deb5b27b85f50bacea5f17b9b1becc
carole tierney <caroleatierney@gmail.com>
2024-02-14 10:57:32 -0500

    needed extensions .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home       from "./routes/Home/Home";
-import About      from "./routes/About/About";
-import Soap       from "./routes/Soap/Soap";
-import SingleSoap from "./routes/Soap/SingleSoap";
-import Header     from "./components/Header";
-import Footer     from "./components/Footer";
+import Home       from "./routes/Home/Home.jsx";
+import About      from "./routes/About/About.jsx";
+import Soap       from "./routes/Soap/Soap.jsx";
+import SingleSoap from "./routes/Soap/SingleSoap.jsx";
+import Header     from "./components/Header.jsx";
+import Footer     from "./components/Footer.jsx";
 import "./index.css";
 
 function App() {
